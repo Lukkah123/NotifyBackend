6f46494a5424959a16da61b3fe9997cd6d596203
Fompe96 <Fompe@hotmail.se>
2022-04-12 19:34:31 +0200

    Small change

--- src/main/java/com/lukka/notifybackend/service/UserService.java
+++ src/main/java/com/lukka/notifybackend/service/UserService.java
@@ -70,6 +70,7 @@ public class UserService {
 
     // Functions to add / remove notes to / from a user
     public User addNoteToUser(String email, Note note) {
+        noteService.save(note);
         User user = getUser(email);
         user.addNote(note);
         return user;
